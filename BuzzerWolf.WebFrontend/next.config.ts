209ba58013ea90bad59316e4784c2651a5dd2036
AzariahAeroborn <26804378@users.noreply.github.com>
2024-12-29 17:40:41 -0500

    try: adjust node app build process to copy files at the correct step

--- BuzzerWolf.WebFrontend/next.config.ts
+++ BuzzerWolf.WebFrontend/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     output: 'export', // required for NextJS static .net hosting
-    distDir: '../BuzzerWolf.Server/nodeapp'
 };
 
 export default nextConfig;
