4773ce36960e39cc4ab2e66f4a2490b80452be7c
Azariah Aeroborn <26804378+AzariahAeroborn@users.noreply.github.com>
2024-12-29 16:45:23 -0500

    try: configure web front end to build into backend directory, conslidate .gitignore (#12)
    Co-authored-by: AzariahAeroborn <26804378@users.noreply.github.com>

--- BuzzerWolf.WebFrontend/next.config.ts
+++ BuzzerWolf.WebFrontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-  output: 'export', // required for NextJS static .net hosting
+    output: 'export', // required for NextJS static .net hosting
+    distDir: '../BuzzerWolf.Server/nodeapp'
 };
 
 export default nextConfig;
